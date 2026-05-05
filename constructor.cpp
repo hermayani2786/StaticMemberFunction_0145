10859f34eb29ae63bd32bd5c27ac1b67ca04f416
hermayani2786 <herma.yani2786@gmail.com>
2026-05-05 19:10:10 +0700

    menambahkan constructor

--- constructor.cpp
+++ constructor.cpp
@@ -5,12 +5,3 @@ class mahasiswa {
 public:
     mahasiswa(); // Constructor
 };
-
-mahasiswa::mahasiswa() {
-    cout << "Constructor Terpanggil" << endl; // Definisi Constructor
-};
-
-int main() {
-    mahasiswa mhs; // Pembuatan Object
-    return 0;
-}
