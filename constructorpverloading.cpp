2937b5e3650b40b2920474291f1cf05082b4360c
hermayani2786 <herma.yani2786@gmail.com>
2026-05-05 19:25:03 +0700

    definisi hanya NAMA

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -22,3 +22,7 @@ mahasiswa::mahasiswa() {
 mahasiswa::mahasiswa(int iNim) {
     nim = iNim; // definisi hanya NIM
 }
+
+mahasiswa::mahasiswa(string iNama) {
+    nama = iNama; // definisi hanya NAMA
+}
