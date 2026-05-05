884f8a456d034c76b9d28e6c72842815a039a92d
hermayani2786 <herma.yani2786@gmail.com>
2026-05-05 19:06:15 +0700

    memanggil prosedur printAll

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -49,3 +49,9 @@ int main() {
     Mahasiswa::setNim(9);
     Mahasiswa mhs3("Andi Janu");
     Mahasiswa mhs4("Joko Wahono");
+
+    //memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
